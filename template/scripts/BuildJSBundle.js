99feb76f16d3336b0b59d1aad25188e01721198f
acigacn <tangkunyin@yisi.cn>
2022-05-12 13:17:24 +0800

    增加打包提醒
    Signed-off-by: acigacn <tangkunyin@yisi.cn>

--- template/scripts/BuildJSBundle.js
+++ template/scripts/BuildJSBundle.js
@@ -8,7 +8,9 @@ function buildBundle(platform, appName) {
   shell.rm('-rf', platformOut);
   shell.mkdir('-p', platformOut);
   // 打RN Bundle
-  shell.exec(`react-native bundle --reset-cache --entry-file index.js --platform ${platform} --assets-dest ${platformOut}  --bundle-output ${platformOut}${bundleName} --dev false`);
+  const cmd = `react-native bundle --reset-cache --entry-file index.js --platform ${platform} --assets-dest ${platformOut}  --bundle-output ${platformOut}${bundleName} --dev false`; 
+  shell.echo(`执行${platform}打包：===>\n${cmd}`);
+  shell.exec(cmd);
   // 整合资源打压缩包
   shell.cp('-r', 'res/', platformOut);
   shell.cd(platformOut);
