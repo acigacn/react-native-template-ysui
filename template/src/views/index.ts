09d60e4d3cba09ac4868f0bfa639dcf71f948688
acigacn <aciga2018@126.com>
2022-05-18 19:56:42 +0800

    删除模板中的演示代码
    Signed-off-by: acigacn <aciga2018@126.com>

--- template/src/views/index.ts
+++ template/src/views/index.ts
@@ -1,16 +1,6 @@
 import HomePage from './home';
 import SettingsPage from './settings';
 
-// 演示页面，可以删掉也可以拿来做尝试 :)
-import ListViewDemo from './examples/ListView';
-import ListDetailDemo from './examples/ListDetail';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const demoPages = {
-  ListViewDemo,
-  ListDetailDemo,
-};
-
 export const rootPage = 'HomePage';
 
 export default {
