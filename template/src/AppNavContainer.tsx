9c9d4872819adb8118d8f162841f2622f6200630
acigacn <aciga2018@126.com>
2022-05-18 15:37:27 +0800

    统一安卓中的导航样式
    Signed-off-by: acigacn <aciga2018@126.com>

--- template/src/AppNavContainer.tsx
+++ template/src/AppNavContainer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Pages, {rootPage} from '@views/index';
 
-
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
